23d2b5d1027c72fb7e8c49e5742732edbf77d80e
jstakun <jstakun@redhat.com>
2021-12-07 12:44:22 +0100

    import fix

--- echo.py
+++ echo.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import socketserver
+import SocketServer
 import socket
 import threading
 import sys
